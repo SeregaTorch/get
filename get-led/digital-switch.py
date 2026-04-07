abc54f4f50584c165b74e5451004a3a21e53a282
anton <acpokerlimonovic@gmail.com>
2026-04-07 16:48:39 +0300

    chinit knopku

--- get-led/digital-switch.py
+++ get-led/digital-switch.py
@@ -6,8 +6,12 @@ GPIO.setup(led, GPIO.OUT)
 button = 13
 GPIO.setup(button, GPIO.IN)
 state = 0
+oleg = 0
 while True:
-    if GPIO.input(button):
+    if GPIO.input(button) and oleg == 0 :
         state = not state
         GPIO.output(led, state)
         time.sleep(0.2)
+        oleg = 1
+    if oleg == 1:
+        # IDK
